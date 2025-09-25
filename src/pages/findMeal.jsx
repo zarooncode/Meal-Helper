e762156053a244306634fc5c8b0e260144ab826f
Zaroon <alizaroon649@gmail.com>
2025-09-25 12:16:23 +0300

    New Changes for Proj, Focusing on FindMeal.jsx

--- src/pages/findMeal.jsx
+++ src/pages/findMeal.jsx
@@ -1,10 +1,19 @@
-import Search from "../components/search"
-import FoodCardForMeals from "../components/mealCard"
+import Search from "../components/search";
+import FoodCardForMeals from "../components/mealCard";
+import RefreshButton from "../components/jumbleMealsButton";
 
 export default function MealPage() {
-  return(
+  return (
     <div className="meal-page">
-      <FoodCardForMeals />
+      <div className="food-cards-container">
+        <div className="extra-container">
+          <RefreshButton />          
+          <Search />
+        </div>
+        <div className="card-container" id="card-containers">
+          <FoodCardForMeals />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
